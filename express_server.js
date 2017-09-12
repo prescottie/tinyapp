00acc6fac2d1f56466ad967c5704f03d22cb6d9c
Scotty <scott.hagan@aol.com>
2017-09-12 21:42:07 +0000

    added alphanumeric generator function to express_server

--- express_server.js
+++ express_server.js
@@ -1,13 +1,30 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+//setting view engine to EJS
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  return Math.floor((1 + Math.random()) * 0x10000000).toString(36);
+}
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  const urlId = generateRandomString();
+  urlDatabase[urlId]  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 app.get("/", (req, res) => {
   res.end("Hello!");
