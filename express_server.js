aaa3f84ef2e4da3ba889037ce8d803ef155492bd
Scotty <scott.hagan@aol.com>
2017-09-12 20:06:04 +0000

    updated urls index and individual url pages to show long and short urls

--- express_server.js
+++ express_server.js
@@ -19,7 +19,10 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id };
+  let templateVars = {
+    shortURL: req.params.id,
+    longUrl: urlDatabase[req.params.id]
+  };
   res.render("urls_show", templateVars);
 });
 
